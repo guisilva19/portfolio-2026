a208119279b39e78c59eef39eb28b42b016b8437
Guilherme Silva Fernandes <2003silvagui@gmail.com>
2026-01-16 23:39:44 -0300

    fix: adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { SocialButtons3D } from "@/components/ui/social-buttons-3d";
 
 export default function Home() {
   return (
-    <div className="relative flex items-center justify-center min-h-screen px-4">
+    <div className="relative flex items-center justify-center h-[100dvh] px-4 overflow-hidden">
       <div className="flex flex-col w-full items-center">
         <h1 className="relative text-6xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold text-foreground lg:pr-40 mb-2 lg:-mb-2 z-[9999] opacity-0 animate-fade-in-up">
           Guilherme
