0149066ed1dc35ca2c0e2f19bd524609f3c5c46a
Guilherme Silva Fernandes <2003silvagui@gmail.com>
2026-03-03 02:30:16 -0300

    fix: remove

--- app/layoutWrapper.tsx
+++ app/layoutWrapper.tsx
@@ -1,23 +1,17 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-rain";
 import TypeAnimation from "@/components/TypeAnimation";
-import TypeAnimationMobile from "@/components/TypeAnimationMobile";
 import ThemeToggle from "@/components/ThemeToggle";
 import Navigation from "@/components/Navigation";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const isHomePage = pathname === "/";
-
   return (
     <>
       <BackgroundBeamsWithCollision className="w-full">
         <div className="w-screen fixed top-0 left-0 z-1">
           <TypeAnimation />
-          {isHomePage && <TypeAnimationMobile />}
         </div>
         <Navigation />
         <ThemeToggle />
